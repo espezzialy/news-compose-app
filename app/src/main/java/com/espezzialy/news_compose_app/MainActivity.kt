3264cc114a2af675d432072eaf9117dbdf029750
Espezzialy Souza <espezzialy@outlook.com>
2024-03-04 11:32:03 -0300

    feat: adding splash screen

--- app/src/main/java/com/espezzialy/news_compose_app/MainActivity.kt
+++ app/src/main/java/com/espezzialy/news_compose_app/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.espezzialy.news_compose_app.ui.theme.NewscomposeappTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             NewscomposeappTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Caralho")
                 }
             }
         }
