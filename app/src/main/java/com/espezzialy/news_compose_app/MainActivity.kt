5e6e425e2ceeb6bfa6a153ff638d8a03da7371e9
Espezzialy Souza <espezzialy@outlook.com>
2024-03-04 15:30:38 -0300

    feat: adding windowtranslucent to navbar and bottombar

--- app/src/main/java/com/espezzialy/news_compose_app/MainActivity.kt
+++ app/src/main/java/com/espezzialy/news_compose_app/MainActivity.kt
@@ -11,12 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.espezzialy.news_compose_app.presentation.onboarding.OnBoardingScreen
 import com.espezzialy.news_compose_app.ui.theme.NewscomposeappTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
         setContent {
             NewscomposeappTheme {
